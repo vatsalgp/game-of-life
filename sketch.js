0fc9b9b41f691d83b5cebdafb9345b6bced21d11
vatsalgp <vatsalgupta280@gmail.com>
2020-04-12 20:17:14 +0530

    added game logic

--- sketch.js
+++ sketch.js
@@ -28,3 +28,67 @@ function mouseClicked() {
     let y = Math.floor(mouseY / cellHeight);
     cells[y][x].click();
 }
+function iterate() {
+    let cellsToToggle = [];
+    for (const col of cells) {
+        for (const cell of col) {
+            let aliveNeighbours = 0;
+            const neighbours = adjacent(cell);
+            for (const neighbour of neighbours) {
+                if (cells[neighbour[0]][neighbour[1]].clicked)
+                    aliveNeighbours++;
+            }
+            if (cell.clicked) {
+                //Cell is ALive
+                if (aliveNeighbours == 2 || aliveNeighbours == 3) {
+                    //Lives next round
+                }
+                else {
+                    //Dies next round
+                    cellsToToggle.push(cell);
+                }
+            } else {
+                //Cell is Dead
+                if (aliveNeighbours == 3) {
+                    //Cell gets born
+                    cellsToToggle.push(cell);
+                }
+            }
+        }
+    }
+    toggleCells(cellsToToggle);
+}
+function adjacent({ x, y }) {
+    let cells = [
+        [y - 1, x + 1],
+        [y - 1, x],
+        [y - 1, x - 1],
+        [y, x - 1],
+        [y, x + 1],
+        [y + 1, x + 1],
+        [y + 1, x],
+        [y + 1, x - 1],
+    ];
+    let output = [];
+    for (const cell of cells) {
+        if (cellExists(cell)) {
+            output.push(cell);
+        }
+    }
+    return output;
+}
+function cellExists(cell) {
+    let y = cell[0], x = cell[1];
+    if (y >= 0 && y < cellsY && x >= 0 && x < cellsX)
+        return true;
+    return false;
+}
+function toggleCells(cells) {
+    for (const cell of cells) {
+        cell.click();
+    }
+}
+function keyPressed() {
+    if (keyCode === ENTER)
+        iterate();
+}
